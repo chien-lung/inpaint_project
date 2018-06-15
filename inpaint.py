e3d4dfb9463760dc427c0168a1d7c3c1b3e63bdc
chien-lung <e125313530@gmail.com>
2018-06-16 03:21:21 +0800

    use matplot draw res[0,1,2] graph

--- inpaint.py
+++ inpaint.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from torchvision.transforms import ToTensor
 from torchvision.transforms import ToPILImage
+import matplotlib.pyplot as plt
 import torch
 
 
@@ -13,13 +14,6 @@ sub_M = ToTensor()
 M = sub_M(mk)
 
 
-'''print(type(I))
-print(I)
-im.show()
-b=ToPILImage()
-I1=b(I)
-I1.show()
-'''
 
 model = torch.load("inpaint.pkl")
 model.eval()
@@ -29,31 +23,24 @@ I1 = I.clone()
 for i in range(3):
     I1[i] = torch.gt( I[i].add(-datamean[i]) , 0 )
     I_in[i] = torch.mul( I[i].add(-datamean[i]) , I1[i] )
-'''
-print(I)
-print(Ip)
-I_img = ToPILImage()
-I_img(I).show()
-Ip_img = ToPILImage()
-Ip_img(Ip).show()
-'''
+
 I_in.masked_fill_( (M.to(torch.uint8).repeat(3,1,1)) , 0 )
 #I_img = ToPILImage()
 #I_img(I_in).show()
 
 input_ts = torch.cat( (I_in,M) ,0)
-#print(input_ts.size())
 input_ts = torch.reshape(input_ts, (1, input_ts.size(0), input_ts.size(1), input_ts.size(2) ))
-#print(input_ts)
 
 res = model.forward(input_ts).to(torch.float32)[0]
 print(res.size())
 res_sub = res.clone()
 res_img = ToPILImage()
 #res_img(res).show()
+'''
 for i in range(3):
     res_sub[i] = torch.lt( res[i] , 1 )
     res[i] = torch.mul( res_sub[i] , res[i] )
+'''
 #print(res)
 #res_img = ToPILImage()
 res_img(res).show()
@@ -62,9 +49,14 @@ minusM = torch.mul(M,-1).add(1)
 out = torch.mul(I,minusM.repeat(3,1,1)).add(torch.mul(res,M.repeat(3,1,1)))
 
 I_out = out.clone()
-'''
-for i in range(3):
-    I_out[i]=out[i].add(datamean[i])
-'''
+
+
 I_img = ToPILImage()
 I_img(I_out).show()
+
+#plt.plot(res[0].detach().numpy())
+#plt.show()
+plt.plot(res[1].detach().numpy())
+plt.show()
+#plt.plot(res[2].detach().numpy())
+#plt.show()
